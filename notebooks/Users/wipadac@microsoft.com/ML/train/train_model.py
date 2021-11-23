f38f5d6df9b8b1b06639c16bac023becc96c5570
Wipada Chanthaweethip <wipadac@microsoft.com>
2021-11-23 03:17:45 +0000

    change alpha to 0.85

--- notebooks/Users/wipadac@microsoft.com/ML/train/train_model.py
+++ notebooks/Users/wipadac@microsoft.com/ML/train/train_model.py
@@ -129,7 +129,7 @@ def train_model(wine_data_path, model_path, alpha, l1_ratio):
 
 # COMMAND ----------
 
-alpha_1 = 0.75
+alpha_1 = 0.85
 l1_ratio_1 = 0.25
 model_path = 'model'
 run_id1 = train_model(wine_data_path=wine_data_path, model_path=model_path, alpha=alpha_1, l1_ratio=l1_ratio_1)
