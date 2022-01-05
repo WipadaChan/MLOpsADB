1a789c1cb293527a0c74c214e661c3b04de85da2
WipadaChan <bizzpicnic@gmail.com>
2022-01-05 12:50:30 +0700

    update slipt delimiter

--- notebooks/Users/wipadac@microsoft.com/ML/deploy/deploy_azure_ml_model.py
+++ notebooks/Users/wipadac@microsoft.com/ML/deploy/deploy_azure_ml_model.py
@@ -165,7 +165,7 @@ primary, secondary = webservice.get_keys()
 # COMMAND ----------
 
 print(dev_scoring_uri)
-newrest = "uri-" + dev_scoring_uri + "|"+ "key-"+ primary
+newrest = "uri;" + dev_scoring_uri + "|"+ "key;"+ primary
 # COMMAND ----------
 
 import json
